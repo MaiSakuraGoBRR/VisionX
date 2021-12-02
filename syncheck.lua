6e08c571dcadafa04443a9e1b813746e7a3e55af
MaiSakuraGoBRR <81828339+MaiSakuraGoBRR@users.noreply.github.com>
2021-12-02 10:55:43 -0800

    Update syncheck.lua

--- syncheck.lua
+++ syncheck.lua
@@ -1,3 +1,7 @@
 -- being a sped is not cool! thank you for using a hand made obfuscator!
 
-return(function(a,b,c,d,e,f,g)local IllIlIIIlIllIlIIIl={[1]=b,[2]=g};local llIlIIIlIlllIlIIIl={[1]=f,[2]=e};local llIlIIIlIllIlIIIlI={[1]=c("Syn Certified"),[2]=a or d};if(IllIlIIIlIllIlIIIl[1])then llIlIIIlIllIlIIIlI[2].herionSynChecked=llIlIIIlIlllIlIIIl[2]else if(IllIlIIIlIllIlIIIl[2])then llIlIIIlIllIlIIIlI[2].herionSynChecked=llIlIIIlIlllIlIIIl[1]end;end)(getgenv,syn,print,getgenv,true,false,syn)
+if syn then
+    getgenv().synapsexcheckernotanipgrabberthisislongsopeoplewontbeabletocrackmystuff = true
+  else
+    getgenv().synapsexcheckernotanipgrabberthisislongsopeoplewontbeabletocrackmystuff = fals
+end
