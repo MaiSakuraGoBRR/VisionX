1be9d9684df8238c18726daf8c25d16ef4741946
hashedv2 <81828339+MaiSakuraGoBRR@users.noreply.github.com>
2021-12-03 10:01:23 -0800

    Update gamechecker.lua

--- gamechecker.lua
+++ gamechecker.lua
@@ -1,5 +1,10 @@
---\\ START OF SCRIPT 12/2/21 //-- 
+--\\ START OF SCRIPT //-- 
 
+local runservice = game:GetService("RunService")
+local players = game.Players
+local lpv = players.LocalPlayer
+local lpname = lpv.Name
+local lpchar = lpv.Character
 local placeidv = game.PlaceId
 local gamesv = 
 { 
@@ -10,4 +15,4 @@ if placeidv == gamesv["Bitcoin Miner"] then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/MaiSakuraGoBRR/VisionX/main/BitcoinMiner.lua", true))()
 end
 
---\\ END OF SCRIPT 12/3/21 //-- 
+--\\ END OF SCRIPT //-- 
