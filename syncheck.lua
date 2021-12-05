dfaad49072aa11c33eb3ecb302cf6f35374abdda
hashedv2 <81828339+MaiSakuraGoBRR@users.noreply.github.com>
2021-12-05 13:05:45 -0800

    Delete syncheck.lua

--- syncheck.lua
+++
@@ -1,7 +0,0 @@
--- being a sped is not cool! thank you for using a hand made obfuscator!
-
-if syn then
-    getgenv().synapsexcheckernotanipgrabberthisislongsopeoplewontbeabletocrackmystuff = true
-  else
-    getgenv().synapsexcheckernotanipgrabberthisislongsopeoplewontbeabletocrackmystuff = fals
-end
